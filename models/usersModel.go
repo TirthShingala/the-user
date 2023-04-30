bb2ed6e2ea26298cc20f4f6553403578f1316ed2
Tirth Shingala <teerthshingala08@gmail.com>
2023-04-30 17:55:21 +0530

    db email index added

--- models/usersModel.go
+++ models/usersModel.go
@@ -1,12 +1,28 @@
 package models
 
 import (
+	"context"
+	"fmt"
 	"time"
 
 	"github.com/TirthShingala/the-user/database"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func init() {
+	var indexModel = mongo.IndexModel{
+		Keys: bson.D{{Key: "email", Value: 1}},
+	}
+
+	var name, err = UserCollection.Indexes().CreateOne(context.TODO(), indexModel)
+	if err != nil {
+		fmt.Println("Index creation error", err)
+	}
+
+	fmt.Println("Name of Index Created: " + name)
+}
 
 type User struct {
 	ID            primitive.ObjectID `json:"id" bson:"_id"`
